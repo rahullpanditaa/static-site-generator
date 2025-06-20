7bb5c2a2dc01418cdbfcc152e5fea4dfcf44c43b
Rahul Pandita <rpandita1999@outlook.com>
2025-06-20 02:24:40 +0000

    creating a text node works

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    text_node = TextNode("anchor text", TextType.LINK, "www.ddd.com")
+    print(text_node)
+
+if __name__ == "__main__":
+    main()
