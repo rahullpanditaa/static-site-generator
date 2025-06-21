b902137d499705dcf1d2e5b501b3c65d6d22b9f2
Rahul Pandita <rpandita1999@outlook.com>
2025-06-21 07:12:56 +0000

    all tests pass

--- src/test_text_to_text_nodes.py
+++ src/test_text_to_text_nodes.py
@@ -6,4 +6,15 @@ from texttype import TextType
 class TestTextToTextNodes(unittest.TestCase):
     def test_text_to_textnodes(self):
         text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-        print(text_to_textnodes(text))
+        self.assertListEqual(text_to_textnodes(text), [
+    TextNode("This is ", TextType.TEXT),
+    TextNode("text", TextType.BOLD),
+    TextNode(" with an ", TextType.TEXT),
+    TextNode("italic", TextType.ITALIC),
+    TextNode(" word and a ", TextType.TEXT),
+    TextNode("code block", TextType.CODE),
+    TextNode(" and an ", TextType.TEXT),
+    TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+    TextNode(" and a ", TextType.TEXT),
+    TextNode("link", TextType.LINK, "https://boot.dev"),
+])
