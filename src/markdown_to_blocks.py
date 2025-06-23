1e94bb5215c9ad26e77e51d48363cb8de461dd74
Rahul Pandita <rpandita1999@outlook.com>
2025-06-23 02:26:18 +0000

    all tests pass

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -2,4 +2,8 @@
 def markdown_to_blocks(markdown):
     blocks_ = markdown.split("\n\n")
     blocks = list(map(lambda s: s.strip(), blocks_))
+    try:
+        blocks.remove("")
+    except ValueError:
+        pass
     return blocks
--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -19,3 +19,26 @@ This is the same paragraph on a new line
                              "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                              "- This is a list\n- with items"
                          ])
+
+
+    def test_markdown_to_blocks_two(self):
+        md = """
+# This is a heading
+
+This is a paragraph of text with some **bold** and _italic_ text
+Same paragraph continued
+
+
+- List item one
+- List item two
+- List item three
+
+"""
+
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(blocks,
+                         [
+                             "# This is a heading",
+                             "This is a paragraph of text with some **bold** and _italic_ text\nSame paragraph continued",
+                             "- List item one\n- List item two\n- List item three"
+                         ])
