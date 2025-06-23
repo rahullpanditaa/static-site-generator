7a631e7773c5fb61e5c1569889e93d1f20dd185e
Rahul Pandita <rpandita1999@outlook.com>
2025-06-23 06:34:18 +0000

    create functions to generate a HTML node from an entire markdown document

---
+++ src/markdown_to_html_node.py
@@ -0,0 +1,53 @@
+from markdown_to_blocks import markdown_to_blocks
+from block_to_block_type import block_to_block_type
+from blocktype import BlockType
+from parentnode import ParentNode
+from text_to_textnodes import text_to_textnodes
+from text_node_to_html_node import text_node_to_html_node
+
+# converts an entire md doc into a single parent HTMLNode
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown) # list of all blocks
+
+    # one parent HTML node will be generated from an entire
+    # markdown document
+    # every block will be a parent node
+    # the inline elements will be leaf nodes and have no children 
+    main_html_node_children = []
+
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        opening_tag = generate_opening_tag(block_type)
+        closing_tag = opening_tag.replace("<", "</")
+        if block_type == BlockType.CODE:
+            code_node = ParentNode("code", [])
+            node = ParentNode("pre", [code_node])
+            main_html_node_children.append(node)
+        children = text_to_children(block)
+        node = ParentNode(opening_tag, children)
+        main_html_node_children.append(node)
+
+    return ParentNode("div", main_html_node_children)
+
+
+
+def text_to_children(text):
+    children_nodes = text_to_textnodes(text)
+    children_html_nodes = [text_node_to_html_node(child_node) for child_node in children_nodes]
+    return children_html_nodes
+
+
+def generate_opening_tag(block_type):
+    match block_type:
+        case BlockType.QUOTE:
+            return "<blockquote>"
+        case BlockType.CODE:
+            return "<pre><code>"
+        case BlockType.HEADING:
+            return "<h>"
+        case BlockType.PARAGRAPH:
+            return "<p>"
+        case BlockType.UNORDERED_LIST:
+            return "<ul>"
+        case BlockType.ORDERED_LIST:
+            return "<ol>"
