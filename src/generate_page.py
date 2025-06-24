6dc61b66d3df3f909b84cb48e0acc5c7363149f8
Rahul Pandita <rpandita1999@outlook.com>
2025-06-24 05:02:38 +0000

    create function to generate a html page from markdown

---
+++ src/generate_page.py
@@ -0,0 +1,22 @@
+import os
+from markdown_to_html_node import markdown_to_html_node
+from extract_title import extract_title
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    source_file_path = os.path.abspath(from_path)
+    template_file_path = os.path.abspath(template_path)
+    dest_file_path = os.path.abspath(dest_path)
+
+    with open(source_file_path, "r") as source, open(template_file_path, "r") as template, open(dest_file_path, "w") as destination:
+        source_file_contents = source.read() # markdown
+        template_file_contents = template.read() # html
+
+        source_node_string = markdown_to_html_node(source_file_contents).to_html() # source md -> html
+        source_title = extract_title(source_file_contents)
+        template_file_contents = template_file_contents.replace("{{ Title }}", source_title).replace("{{ Content }}", source_node_string)
+
+        # write to a file at dest_path
+        destination.write(template_file_contents)
