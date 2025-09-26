a103609c3dc7220acaaebb20503b812783abeb03
Rahul Pandita <rpandita1999@outlook.com>
2025-09-26 02:06:46 +0000

    create a TextType enum to hold types of inline text/text nodes; TextNode class to represent a text node

---
+++ src/textnode.py
@@ -0,0 +1,41 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "url"
+
+class TextNode:
+    def _init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    @property
+    def text(self):
+        return self._text
+    
+    @text.setter
+    def text(self, text):
+        if not text:
+            raise ValueError("Error: Missing text content for the node")
+        self._text = text
+
+    @property
+    def text_type(self):
+        return self._text_type
+    
+    @text_type.setter
+    def text_type(self, text_type):
+        if text_type not in [t.name for t in TextType]:
+            raise ValueError(f"Error: Invalid text type {text_type} given")
+        self._text_type = text_type
+
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
