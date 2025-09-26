8c61992282c0fcd365d605fc7f8aeee790196a15
Rahul Pandita <rpandita1999@outlook.com>
2025-09-26 07:57:46 +0000

    create function to convert text node to html node (Leaf Node)

---
+++ src/project/textnode_to_htmlnode.py
@@ -0,0 +1,21 @@
+from project.textnode import TextNode, TextType
+from project.htmlnode import HTMLNode
+from project.leafnode import LeafNode
+
+def text_node_to_html_node(text_node: TextNode):
+    if text_node.text_type not in [t.value for t in TextType]:
+        raise ValueError("Error: Given Text Node has invalid type")
+    
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag="b", value=text_node.text)
+        case TextType.ITALIC:
+            return LeafNode(tag="i", value=text_node.text)
+        case TextType.CODE:
+            return LeafNode(tag="code", value=text_node.text)
+        case TextType.LINK:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
