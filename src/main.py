231f72d75f1b15ff47d0f87e87a727c385e895ff
Rahul Pandita <rpandita1999@outlook.com>
2025-09-27 06:06:35 +0000

    create function to extract links and images from markdown text

--- src/main.py
+++ src/main.py
@@ -3,10 +3,7 @@ from project.htmlnode import HTMLNode
 from project.split_nodes_delimiter import split_nodes_delimiter
 
 def main():
-    node = TextNode("This is text with `code block` inside it.", TextType.PLAIN)
-    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-    print(new_nodes)
-    print(TextType.CODE)
+    ...
 
 if __name__ == "__main__":
     main()
