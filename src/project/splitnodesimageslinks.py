73fc1a14c9e3041459fe1b96f0b51e453385e6d9
Rahul Pandita <rpandita1999@outlook.com>
2025-09-29 04:38:46 +0000

    create function to extract img nodes from markdown

--- src/project/splitnodesimageslinks.py
+++ src/project/splitnodesimageslinks.py
@@ -6,7 +6,8 @@ def split_nodes_image(old_nodes: list[TextNode]):
     for node in old_nodes:
         old_node_text = node.text
         if node.text_type != TextType.PLAIN:
-            new_nodes.append(node)
+            node_text = old_node_text.replace("`", "").replace("**", "").replace("_", "")
+            new_nodes.append(TextNode(node_text, node.text_type))
             continue
         if old_node_text and not extract_markdown_images(old_node_text):
             new_nodes.append(node)
@@ -32,5 +33,31 @@ def split_nodes_image(old_nodes: list[TextNode]):
 
         
 
-def split_nodes_links(old_nodes):
-    ...
+def split_nodes_links(old_nodes: list[TextNode]):
+    new_nodes = []
+    for node in old_nodes:
+        old_node_text = node.text
+        if node.text_type != TextType.PLAIN:
+            node_text = old_node_text.replace("`", "").replace("**", "").replace("_", "")
+            new_nodes.append(TextNode(node_text, node.text_type))
+            continue
+        if old_node_text and not extract_markdown_links(old_node_text):
+            new_nodes.append(node)
+            continue
+
+        text_links = extract_markdown_links(old_node_text)
+
+        for link in text_links:
+            current_node_contents = old_node_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(current_node_contents) != 2:
+                raise ValueError("Error: Invalid markdown.")
+            if current_node_contents[0]:
+                new_nodes.append(TextNode(current_node_contents[0], TextType.PLAIN))
+            link_node = TextNode(link[0], TextType.LINK, link[1])
+            new_nodes.append(link_node)
+            old_node_text = current_node_contents[1]
+
+        if old_node_text:
+            new_nodes.append(TextNode(old_node_text), TextType.PLAIN)
+        
+    return new_nodes
