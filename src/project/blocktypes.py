b77e88c72aaea0449bbdf846423c43be171af668
Rahul Pandita <rpandita1999@outlook.com>
2025-09-29 06:59:22 +0000

    create an enum for different types of markdown text blocks

---
+++ src/project/blocktypes.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "ul list"
+    ORDERED_LIST = "ol_list"
