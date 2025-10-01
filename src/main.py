3a05d4cd9138c5865a5475a3893b72aaa00b2f9f
Rahul Pandita <rpandita1999@outlook.com>
2025-10-01 06:35:04 +0000

    run generator function to copy everything new from static to public dir

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from project.generatepage import generate_page
 
 def main():
     src_to_dest()   
-    generate_page("content/index.md", "template.html", "public/index.html")
+    # generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
