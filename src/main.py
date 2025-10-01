2319c6976c727722eef56305a7965dca2e6e33ec
Rahul Pandita <rpandita1999@outlook.com>
2025-10-01 05:13:54 +0000

    create function to copy contents from src to dest dir

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 from project.textnode import TextNode, TextType
 from project.htmlnode import HTMLNode
 from project.split_nodes_delimiter import split_nodes_delimiter
+from pathlib import Path
+from shutil import copytree
 
 def main():
     ...
+
+
+
+
+
+
+
+    
+
 
 if __name__ == "__main__":
     main()
---
+++ src/project/cp_src_to_dest.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from shutil import copytree
+
+def src_to_dest(src="static", dest="public"):
+    src_dir = Path(src).resolve()
+    dest_dir = Path(dest).resolve()
+
+    if not (src_dir.exists() or dest_dir.exists()):
+        return "Error: Invalid directories given"
+    
+    if not (src_dir.is_dir() and dest_dir.is_dir()):
+        return "Error: Given args not directories"
+    
+    # delete contents of dest dir
+    delete_dir_contents(dest_dir)
+    copytree(src=src_dir, dst=dest_dir)
+    
+
+# will delete all files in dir and subdir
+# at the end of it the dir will only contain empty subdirs
+def delete_dir_contents(dir: Path):
+    # root -> pth to dir currently being walked
+    # dirs -> list of str for names of subdirs in root
+    # files -> list of str for files in root
+    for root, dirs, files in dir.walk(top_down=False):
+        for name in files:
+            (root / name).unlink()
+        for name in dirs:
+            (root / name).rmdir()
+        
